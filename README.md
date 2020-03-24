01724b490bbea2bd9e1d2c9a7a08e1e61c090799
D3473R <d3473r@gmail.com>
2020-03-24 20:10:37 +0100

    Use uuids as invite room names. Add validations

--- README.md
+++ README.md
@@ -14,11 +14,9 @@
 
 - `JITSI_SECRET` with the shared secret from jitsi `JWT_APP_SECRET`. If you don't have one already run: `node -e "console.log(require('crypto').randomBytes(24).toString('base64'));"` again.
 - `DEFAULT_ROOM` with a default room name e.g. `meeting`
-- `INVITE_ROOM_PREFIX` with a invite room prefix e.g. `meetingInvite`
+- `INVITE_ROOM_PREFIX` with a invite room prefix e.g. `meeting-invite`
 - `JITSI_URL` with the url of your jitsi server e.g. `https://meet.example.com`
 - `JITSI_SUB` with your jitsi server sub e.g. `meet.example.com`
-
-**Important:** Dashes in the room name are not yet possible with [docker-jitsi-meet](https://github.com/jitsi/docker-jitsi-meet/pull/153)
 
 ## Run
 
--- client/src/App.vue
+++ client/src/App.vue
@@ -24,23 +24,25 @@
             <v-card>
               <v-card-title>Login</v-card-title>
               <v-card-text>
-                <v-row>
-                  <v-col>
-                    <v-text-field label="First Name" :value="profile.firstName" disabled></v-text-field>
-                  </v-col>
-                  <v-col>
-                    <v-text-field label="Last Name" :value="profile.lastName" disabled></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col>
-                    <v-text-field label="Email" :value="profile.email" disabled></v-text-field>
-                  </v-col>
-                  <v-col>
-                    <v-text-field label="Room" v-model="room"></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-btn color="primary" v-on:click="openJitsi">Open Jitsi</v-btn>
+                <v-form v-model="roomValid">
+                  <v-row>
+                    <v-col>
+                      <v-text-field label="First Name" :value="profile.firstName" disabled></v-text-field>
+                    </v-col>
+                    <v-col>
+                      <v-text-field label="Last Name" :value="profile.lastName" disabled></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col>
+                      <v-text-field label="Email" :value="profile.email" disabled></v-text-field>
+                    </v-col>
+                    <v-col>
+                      <v-text-field label="Room" v-model="room" :rules="roomRules"></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-btn color="primary" v-on:click="openJitsi" :disabled="!roomValid">Open Jitsi</v-btn>
+                </v-form>
               </v-card-text>
             </v-card>
           </v-col>
@@ -51,63 +53,79 @@
             <v-card>
               <v-card-title>Invite</v-card-title>
               <v-card-text>
-                <v-row>
-                  <v-col md="4">
-                    <v-text-field label="Invite Room Prefix" :value="inviteRoomPrefix" disabled></v-text-field>
-                  </v-col>
-                  <v-col md="8">
-                    <v-text-field label="Invite Room Id" v-model="inviteRoomId"></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col>
-                    <v-list dense>
-                      <v-list-item
-                          v-for="(invite, i) in invites"
-                          :key="i"
-                      >
-                        <v-list-item-content>
-                          <v-text-field label="Name" v-model="invite.name"></v-text-field>
-                        </v-list-item-content>
+                <v-form v-model="inviteValid">
+                  <v-row>
+                    <v-col md="4">
+                      <v-text-field label="Invite Room Prefix" :value="inviteRoomPrefix" disabled></v-text-field>
+                    </v-col>
+                    <v-col md="8">
 
-                        <v-list-item-action>
-                          <v-row>
-                            <v-tooltip bottom>
+                      <v-tooltip left>
+                        <template v-slot:activator="{ on }">
+                          <v-text-field label="Invite Room Id" v-model="inviteRoomUuid" :rules="uuidRules"
+                                        @paste="onPasteRoom" v-on="on">
+                            <v-tooltip bottom slot="append">
                               <template v-slot:activator="{ on }">
-                                <v-btn icon v-on:click="copyInviteLink(invite.name)" v-on="on"
-                                       v-bind:disabled="invite.name.length < 3">
-                                  <v-icon>mdi-content-copy</v-icon>
-                                </v-btn>
+                                <v-icon v-on:click="inviteRoomUuid=generateUuid()" v-on="on">mdi-reload</v-icon>
                               </template>
-                              <span>Copy invite link</span>
+                              <span>Generate Uuid</span>
                             </v-tooltip>
-                            <v-tooltip bottom>
-                              <template v-slot:activator="{ on }">
-                                <v-btn icon v-on:click="$delete(invites, i)" v-on="on">
-                                  <v-icon>mdi-delete</v-icon>
-                                </v-btn>
-                              </template>
-                              <span>Delete invite</span>
-                            </v-tooltip>
-                          </v-row>
-                        </v-list-item-action>
-                      </v-list-item>
-                    </v-list>
-                  </v-col>
-                </v-row>
-                <v-row justify="end">
-                  <v-col md="1">
-                    <v-tooltip bottom>
-                      <template v-slot:activator="{ on }">
-                        <v-btn icon v-on:click="invites.push({name: ''})" v-on="on">
-                          <v-icon>mdi-plus</v-icon>
-                        </v-btn>
-                      </template>
-                      <span>Add invite</span>
-                    </v-tooltip>
-                  </v-col>
-                </v-row>
-                <v-btn color="primary" v-on:click="joinInvite">Join Invite</v-btn>
+                          </v-text-field>
+                        </template>
+                        <span>Paste the meeting url to invite more people</span>
+                      </v-tooltip>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="invites.length > 0">
+                    <v-col>
+                      <v-list dense>
+                        <v-list-item
+                            v-for="(invite, i) in invites"
+                            :key="i"
+                        >
+                          <v-list-item-content>
+                            <v-text-field label="Name" v-model="invite.name"></v-text-field>
+                          </v-list-item-content>
+
+                          <v-list-item-action>
+                            <v-row>
+                              <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                  <v-btn icon v-on:click="copyInviteLink(invite.name)" v-on="on"
+                                         v-bind:disabled="invite.name.length < 3">
+                                    <v-icon>mdi-content-copy</v-icon>
+                                  </v-btn>
+                                </template>
+                                <span>Copy invite link</span>
+                              </v-tooltip>
+                              <v-tooltip bottom>
+                                <template v-slot:activator="{ on }">
+                                  <v-btn icon v-on:click="$delete(invites, i)" v-on="on">
+                                    <v-icon>mdi-delete</v-icon>
+                                  </v-btn>
+                                </template>
+                                <span>Delete invite</span>
+                              </v-tooltip>
+                            </v-row>
+                          </v-list-item-action>
+                        </v-list-item>
+                      </v-list>
+                    </v-col>
+                  </v-row>
+                  <v-row justify="end">
+                    <v-col md="1">
+                      <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                          <v-btn icon v-on:click="invites.push({name: ''})" v-on="on">
+                            <v-icon>mdi-plus</v-icon>
+                          </v-btn>
+                        </template>
+                        <span>Add invite</span>
+                      </v-tooltip>
+                    </v-col>
+                  </v-row>
+                  <v-btn color="primary" v-on:click="joinInvite" :disabled="!inviteValid">Join Invite</v-btn>
+                </v-form>
               </v-card-text>
             </v-card>
           </v-col>
@@ -119,7 +137,7 @@
 
 <script>
   import axios from "axios";
-  import crypto from "crypto";
+  import {v4 as uuidv4} from "uuid";
 
   axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("vue-token")}`;
 
@@ -129,7 +147,7 @@
     props: ["keycloak", "profile"],
     computed: {
       inviteRoom: function () {
-        return `${this.inviteRoomPrefix}${this.inviteRoomId}`;
+        return `${this.inviteRoomPrefix}-${this.inviteRoomUuid}`;
       },
       url() {
         return window.location.href;
@@ -148,15 +166,49 @@
       },
       buildLink: function (room, token) {
         return encodeURI(`${this.config.jitsiUrl}${room}?jwt=${token}`)
+      },
+      generateUuid: () => {
+        return uuidv4();
+      },
+      validateUrl: url => {
+        return /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/.test(url);
+      },
+      validateUuid: uuid => {
+        return /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$/.test(uuid);
+      },
+      onPasteRoom: function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        const clipboardData = event.clipboardData || window.clipboardData;
+        const pastedData = clipboardData.getData("Text");
+
+        if (this.validateUrl(pastedData)) {
+          const uuidString = new URL(pastedData).pathname.replace(`/${this.inviteRoomPrefix}-`, "");
+          if (this.validateUuid(uuidString)) {
+            this.inviteRoomUuid = uuidString;
+          }
+        }
       }
     },
-    data: () => ({
-      config: null,
-      room: null,
-      inviteRoomPrefix: null,
-      inviteRoomId: crypto.randomBytes(24).toString("hex"),
-      invites: []
-    }),
+    data() {
+      return {
+        config: null,
+        room: null,
+        roomValid: true,
+        inviteValid: true,
+        inviteRoomPrefix: null,
+        inviteRoomUuid: uuidv4(),
+        invites: [],
+        roomRules: [
+          v => !!v || "Room is required"
+        ],
+        uuidRules: [
+          v => !!v || "UUID is required",
+          v => this.validateUuid(v) || "Must be a valid uuid",
+        ]
+      }
+    },
     async mounted() {
       this.config = (await axios.get("/api/config")).data;
       this.room = this.config.defaultRoom;
