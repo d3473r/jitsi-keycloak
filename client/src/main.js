4b2e66f3142d7ca2482fb561671764bd098bb4ce
teutat3s <10206665+teutat3s@users.noreply.github.com>
2020-11-18 12:45:37 +0100

    Implement async-await syntactic sugar

--- client/src/main.js
+++ client/src/main.js
@@ -8,12 +8,14 @@ import * as Keycloak from "keycloak-js";
 Vue.use(VueRouter)
 Vue.config.productionTip = false;
 
-axios.get("api/keycloak.json").then(response => {
-  const keycloak = Keycloak({
-    url: response.data["auth-server-url"],
-    realm: response.data["realm"],
-    clientId: response.data["resource"]
-  });
+(async () => {
+  try {
+    const response = await axios.get("api/keycloak.json");
+    const keycloak = Keycloak({
+      url: response.data["auth-server-url"],
+      realm: response.data["realm"],
+      clientId: response.data["resource"]
+    });
 
 
     const router = new VueRouter({
@@ -21,7 +23,7 @@ axios.get("api/keycloak.json").then(response => {
       routes: [{path: "/:room", component: App}]
     });
 
-  keycloak.init({onLoad: "login-required", promiseType: "native", checkLoginIframe: false}).then(auth => {
+    const auth = await keycloak.init({onLoad: "login-required", promiseType: "native", checkLoginIframe: false});
     if (!auth) {
       window.location.reload();
     } else {
@@ -31,35 +33,23 @@ axios.get("api/keycloak.json").then(response => {
     localStorage.setItem("vue-token", keycloak.token);
     localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
 
-    keycloak.loadUserProfile()
-        .then(profile => {
-          new Vue({
-            vuetify,
-            router,
-            render: h => h(App, {props: {keycloak: keycloak, profile: profile}})
-          }).$mount("#app");
+    const profile = await keycloak.loadUserProfile();
+    new Vue({
+      vuetify,
+      router,
+      render: h => h(App, {props: {keycloak: keycloak, profile: profile}})
+    }).$mount("#app");
 
-          setTimeout(() => {
-            keycloak.updateToken(70).then(refreshed => {
-              if (refreshed) {
-                console.debug("Token refreshed" + refreshed);
-              } else {
-                console.warn("Token not refreshed, valid for "
-                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + " seconds");
-              }
-            }).catch(() => {
-              console.error("Failed to refresh token");
-            });
-          }, 60000)
-        }).catch(function () {
-      console.error("Failed to load user profile");
-    });
-  }).catch(() => {
-    console.error("Authenticated Failed");
-  });
-});
-
-
-
-
-
+    setTimeout(async () => {
+      const refreshed = await keycloak.updateToken(70);
+      if (refreshed) {
+        console.debug("Token refreshed" + refreshed);
+      } else {
+        console.warn("Token not refreshed, valid for "
+            + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + " seconds");
+      }
+    }, 60000)
+  } catch(e) {
+    console.error("Authenticated Failed: ", e);
+  }
+})();
