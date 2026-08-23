ae14832f5fcaa93e8016afdf1d425d346043d7cc
d3473r <d3473r@gmail.com>
2026-08-24 00:36:33 +0200

    Change keycloak config path to /config and rename to compose.yml

--- server/utils/env.ts
+++ server/utils/env.ts
@@ -5,6 +5,6 @@ export function useEnvConfig() {
     jitsiUrl: process.env.JITSI_URL || 'JITSI_URL',
     allowedSub: process.env.ALLOWED_SUB || '*',
     allowedRoom: process.env.ALLOWED_ROOM || '*',
-    keycloakConfigPath: process.env.KEYCLOAK_CONFIG_PATH || './config/keycloak.json',
+    keycloakConfigPath: process.env.KEYCLOAK_CONFIG_PATH || '/config/keycloak.json',
   }
 }
